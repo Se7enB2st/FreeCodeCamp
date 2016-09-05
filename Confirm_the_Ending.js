c136157c429737fbe0c3caff7c4f36f2a0309f61
Ethan He <ethanhe1992@gmail.com>
2016-09-05 16:42:35 -0400

    Update Confirm_the_Ending.js

--- Confirm_the_Ending.js
+++ Confirm_the_Ending.js
@@ -3,18 +3,18 @@
 //Check if a string (first argument, str) ends with the given target string (second argument, target).
 
 function confirmEnding(str, target) {
-  if (str.substr(-target.length) == target) 
-      return true;
-  } else {
-    return false;
-  }
+    if (str.substr(-target.length) == target) 
+        return true;
+    } else {
+        return false;
+    }
 }
 
 confirmEnding("Bastian", "n"); //true
 
 //ES2015 endsWith method 
 function confirmEnding(str, target) {
-	return str.endsWith(target);
+    return str.endsWith(target);
 }
 
 confirmEnding("Bastian", "n"); //true
