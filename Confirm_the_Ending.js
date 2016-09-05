77c51305a2d75cbd6eca06b355784f0931bd092d
Ethan He <ethanhe1992@gmail.com>
2016-09-04 21:40:34 -0400

    Create Confirm_the_Ending.js

---
+++ Confirm_the_Ending.js
@@ -0,0 +1,13 @@
+//FreeCodeCamp Bonfire Challenge: Confirm the Ending
+
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+  if (str.substr(-target.length) == target) 
+      return true;
+  } else {
+    return false;
+  }
+}
+
+confirmEnding("Bastian", "n"); //true
