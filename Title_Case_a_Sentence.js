c5b59cddb4877505c92d2218718343387ca6f01d
Ethan He <ethanhe1992@gmail.com>
2016-09-04 22:04:11 -0400

    Update Title_Case_a_Sentence.js

--- Title_Case_a_Sentence.js
+++ Title_Case_a_Sentence.js
@@ -4,13 +4,13 @@
 //For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
-  var pieces = str.split(" ");
-  for ( var i = 0; i < pieces.length; i++ ) {
-    pieces[i] = pieces[i].toLowerCase();
-    var capitalLetter = pieces[i].charAt(0).toUpperCase();
-    pieces[i] = capitalLetter + pieces[i].substr(1); // Capital Letter + letters start at position[1]
-    }  
-  return pieces.join(" ");
+    var pieces = str.split(" ");
+    for ( var i = 0; i < pieces.length; i++ ) {
+        pieces[i] = pieces[i].toLowerCase();
+        var capitalLetter = pieces[i].charAt(0).toUpperCase();
+        pieces[i] = capitalLetter + pieces[i].substr(1); // Capital Letter + letters start at position[1]
+        }  
+    return pieces.join(" ");
 }
 
 titleCase("sHoRt AnD sToUt"); //"Short And Stout"
