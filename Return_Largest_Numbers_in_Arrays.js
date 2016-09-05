c3ee702e7d264a81c061353e60ed9cc3e854850e
Ethan He <ethanhe1992@gmail.com>
2016-09-04 22:03:18 -0400

    Update Return_Largest_Numbers_in_Arrays.js

--- Return_Largest_Numbers_in_Arrays.js
+++ Return_Largest_Numbers_in_Arrays.js
@@ -4,11 +4,11 @@
 //For simplicity, the provided array will contain exactly 4 sub-arrays.
 
 function largestOfFour(arr) {
-  var maxArray = [];
-  for (var i=0;i < 4; i++) {
-    maxArray.push(Math.max.apply(Math, arr[i])); //Math.max.apply take arr as argument and apply to 'Math', then find the largest number
-  }
-  return maxArray;
+    var maxArray = [];
+  	for (var i=0;i < 4; i++) {
+    	maxArray.push(Math.max.apply(Math, arr[i])); //Math.max.apply take arr as argument and apply to 'Math', then find the largest number
+  	}
+  	return maxArray;
 }
 
 largestOfFour([[13, 27, 18, 26],[4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); //[27,5,39,1001]
