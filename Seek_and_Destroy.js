100c05cb4a6d0b060730a3ef61b0c3061606a54d
Ethan He <ethanhe1992@gmail.com>
2016-09-10 12:41:03 -0400

    Update Seek_and_Destroy.js

--- Seek_and_Destroy.js
+++ Seek_and_Destroy.js
@@ -3,11 +3,11 @@
 //You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. 
 //Remove all elements from the initial array that are of the same value as these arguments.
 
-function destroyer(arr, a, b, c) {
-    var base = arr.filter(function(x) {
-      return x !== a && x !== b && x !== c;
+function destroyer(arr, a, b, c) { // ([1,2,3,1,2,3],2,3,undefined)
+    var base = arr.filter(function(x) { 
+      return x !== a && x !== b && x !== c; //compare and filter out a, b, c  
     });
-    return base;
+    return base; 
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([1, 2, 3, 1, 2, 3], 2, 3); //[1,1]
