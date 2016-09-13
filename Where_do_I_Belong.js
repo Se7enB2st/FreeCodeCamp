686db20775a185073b05c1bc0b3eb92c48761e94
Ethan He <ethanhe1992@gmail.com>
2016-09-12 22:34:51 -0400

    Create Where_do_I_Belong.js

---
+++ Where_do_I_Belong.js
@@ -0,0 +1,21 @@
+//FreeCodeCamp Bonfire Challenge: Where do I Belong
+
+//Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted.
+//The returned value should be a number.
+
+//For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+
+function getIndexToIns(arr, num) { //sort the array in ascending order
+    arr.sort(function(a, b) { 
+        return a - b;
+    });
+    for(var index = 0; index < arr.length; index++) {
+        if(num <= arr[index]) {
+            break;
+        }
+    }
+    return index;
+}
+
+getIndexToIns([85, 100, 35, 45, 60], 50); //2
